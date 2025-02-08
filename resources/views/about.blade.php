e5d59169a87326e3f29b89a10f1b9efacbf34482
Nguyen Trong Hieu <hieunguyentronghieu69@gmail.com>
2025-02-08 21:34:41 +0700

    first commit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    @vite('resources/css/app.css')
+    <title>Document</title>
+</head>
+<body class="antialiased bg-gray-200">
+    <p class="text-gray-500 bg-blue-500">About Page</p>
+</body>
+</html>
